eebdec3d3ddb0767dac648fc222180bbf36143f6
Brandon Wright <brandon@developish.com>
2018-01-01 16:12:56 -0700

    Save new names

--- src/App.js
+++ src/App.js
@@ -4,18 +4,73 @@ import './App.css';
 
 const Person = (props) => <div className="FamilyMember">{props.name}</div>
 
+class PersonForm extends Component {
+  constructor(props) {
+    super()
+
+    this.addMember = props.addMember
+    this.state = { value: '' }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    this.props.addMember(this.state.value)
+    this.setState({ value: '' })
+    event.preventDefault()
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="text" vaule={this.state.value} onChange={this.handleChange} />
+        </label>
+
+        <input type="submit" value="Add Family Member" />
+      </form>
+    )
+  }
+}
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
       familyMembers: []
     }
+
+    this.addMember = this.addMember.bind(this)
   }
 
   componentWillMount() {
     fetch('http://localhost:5000/members')
       .then(response => response.json())
       .then(members => this.setState({ familyMembers: members }))
+  }
+
+  addMember(name) {
+    let data = { name: name }
+    var headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+
+    fetch('http://localhost:5000/members', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: headers
+    })
+    .then(response => {
+      if (response.ok) {
+        this.setState({ familyMembers: [...this.state.familyMembers, name] })
+      } else {
+        alert('There was an error saving this member name')
+      }
+    })
   }
 
   render() {
@@ -33,7 +88,7 @@ class App extends Component {
           )}
         </div>
 
-        <button>Add Family Member</button>
+        <PersonForm addMember={this.addMember}/>
       </div>
     );
   }
